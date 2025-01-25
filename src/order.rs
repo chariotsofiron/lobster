893cdf5e950980aa68570022e5585fb9ccec41b9
chariotsofiron <123149780+chariotsofiron@users.noreply.github.com>
2025-01-25 12:14:02 -0500

    remove unnecessary trait bounds on order associated types

--- src/order.rs
+++ src/order.rs
@@ -4,20 +4,23 @@ use core::ops::Sub;
 
 /// An order in an order book.
 pub trait Order: Clone {
-    type OrderId: Eq + Hash + Clone;
-    type Quantity: Copy + Ord + Sub<Output = Self::Quantity>;
+    type OrderId: Eq + Hash;
+    type Quantity: Ord + Sub<Output = Self::Quantity>;
     type Price: Ord;
 
     fn id(&self) -> Self::OrderId;
+
     fn quantity(&self) -> Self::Quantity;
+
+    fn price(&self) -> Self::Price;
+
+    /// Returns `true` if the order is a buy order, `false` if it is a sell order.
+    fn is_buy(&self) -> bool;
+
     fn set_quantity(&mut self, quantity: Self::Quantity);
 
     #[expect(clippy::arithmetic_side_effects)]
     fn reduce_quantity(&mut self, quantity: Self::Quantity) {
         self.set_quantity(self.quantity() - quantity);
     }
-
-    fn price(&self) -> Self::Price;
-    /// Returns `true` if the order is a buy order, `false` if it is a sell order.
-    fn is_buy(&self) -> bool;
 }
